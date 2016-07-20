cc2f667c9d3b0c0b798e4b41b599883b97dfd8fb
Simon <Simon@Mibellaluna>
2016-07-20 17:40:34 -0600

    Commit con la clase de acceso a datos y un ejecutable para stub

--- src/DataAccess/ClienteAccess.java
+++ src/DataAccess/ClienteAccess.java
@@ -13,9 +13,8 @@ import java.util.logging.Logger;
 public class ClienteAccess {
     
     String connectionString=
-                " jdbc:sqlserver: (localhost);" +
-            
-                " databaseName = videoclub_db;" +
+                "jdbc:sqlserver://localhost:1433;"            
+                +" databaseName = videoclub_db;" +
                 "username=sa;"
                 +"Password= Laspelotas9n;";
     
@@ -28,21 +27,35 @@ public class ClienteAccess {
     {
         
         try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
             con = DriverManager.getConnection(connectionString);
             proc = con .prepareCall("exec proc ClienteInsertar (?,?,?,?,?,?)");
+            
+            // los parámetros se establecen de acuerdo al orden en el procedimiento
+            //almacenado. También se tienen que registerOut antes que el 
+            //procedimiento se ejecute
             proc.setString(1, cliente.getNombre());
+            proc.registerOutParameter(1,Types.NVARCHAR);
             proc.setString(2, cliente.getApellidos());
+            proc.registerOutParameter(2,Types.NVARCHAR);
             proc.setString(3, cliente.getCedula());
+            proc.registerOutParameter(3,Types.NVARCHAR);
             proc.setString(4, cliente.getEmail());
+            proc.registerOutParameter(4,Types.NVARCHAR);
             proc.setString(5, cliente.getTelefono());
+            proc.registerOutParameter(5,Types.NVARCHAR);
             proc.setString(6, cliente.getDireccion());
+            proc.registerOutParameter(6,Types.NVARCHAR);
             exito = proc.execute();
             
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ClienteAccess.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(ClienteAccess.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
             Logger.getLogger(ClienteAccess.class.getName()).log(Level.SEVERE, null, ex);
         }
         return true;
